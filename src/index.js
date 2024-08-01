9e04d23c5a6fc486673c5e4208b5a459a10c2df4
mwhitlow83 <mwhitlow83@gmail.com>
2024-08-01 16:49:04 -0500

    works

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { configureStore } from '@reduxjs/toolkit'
+import  { configureStore } from '@reduxjs/toolkit'
 import { Provider} from 'react-redux';
 
 
